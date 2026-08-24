922924c71ada2a713b3757d4cdb4ca7f8c8d4c26
fatratra-png <nyfatratrar@gmail.com>
2026-08-24 21:07:20 +0300

    fix: call adapter() in svelte.config.js, generate .svelte-kit

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,7 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 const config = {
   preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter,
+    adapter: adapter(),
   },
 };
 
